d252b8caf3b6c74f18caa3882bbcfc51e08573b3
apdlwh <146094326+apdlwh@users.noreply.github.com>
2024-02-16 14:36:01 -0500

    Update Assessment.h

--- Week 2/Lab/Assessment.h
+++ Week 2/Lab/Assessment.h
@@ -1,1 +1,23 @@
+#ifndef SDDS_ASSESSMENT_H
+#define SDDS_ASSESSMENT_H
 
+#include <cstdio> 
+
+namespace seneca {
+
+    struct Assessment {
+        double* m_mark;  
+        char* m_title;   
+    };
+
+   
+    bool read(int& value, FILE* fptr);
+    bool read(double& value, FILE* fptr);
+    bool read(char* cstr, FILE* fptr);
+    bool read(Assessment& assess, FILE* fptr);
+    void freeMem(Assessment*& aptr, int size);
+    int read(Assessment*& aptr, FILE* fptr);
+
+} // namespace seneca
+
+#endif // SDDS_ASSESSMENT_H
