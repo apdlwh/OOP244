3956703d977ab90f4f845a0a88a4e3d20768dcb0
apdlwh <146094326+apdlwh@users.noreply.github.com>
2024-02-16 14:44:25 -0500

    Update Customer.h

--- Week 3/Lab/Customer.h
+++ Week 3/Lab/Customer.h
@@ -1,1 +1,28 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
 
+#include "Car.h"
+
+namespace seneca{
+
+class Customer{
+private: 
+    int m_id;
+    char* m_name;
+    const Car* m_car;
+
+public:
+Customer();
+~Customer();
+
+    bool isEmpty() const;
+    void setEmpty();
+    void deallocateMemory();
+    void set(int customerID, const char* name, const Car* car);
+    void display() const;
+    const Car& car() const;
+
+    };
+}
+
+#endif
