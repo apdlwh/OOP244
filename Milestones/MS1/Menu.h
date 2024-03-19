87bfc0fe8b0154f19cfe4cb1ce42b9742bd2f145
apdlwh <146094326+apdlwh@users.noreply.github.com>
2024-03-19 10:20:43 -0400

    Update Menu.h

--- Milestones/MS1/Menu.h
+++ Milestones/MS1/Menu.h
@@ -1,1 +1,35 @@
+#ifndef MENU_H
+#define MENU_H
+#include <iostream>
+#include <cstring>
 
+namespace seneca{
+class Menu{
+private:
+   char* m_text;
+   int m_numOptions;
+   int m_numTabs;
+
+   Menu(const Menu& other) = delete; //prevents copy
+   Menu& operator=(const Menu& other) = delete; //and assignment 
+
+public:
+
+    Menu(const char* menuContent, int numberOfTabs = 0);
+    ~Menu();
+    void display() const;
+    int& operator>>(int& selection);
+
+
+ 
+};
+
+
+
+
+
+
+
+
+}
+#endif
