97ed7c0ac8df8682b305841c7e127f12d050c674
apdlwh <146094326+apdlwh@users.noreply.github.com>
2024-03-19 10:21:15 -0400

    Update Time.h

--- Milestones/MS1/Time.h
+++ Milestones/MS1/Time.h
@@ -1,1 +1,27 @@
+#ifndef TIME_H
+#define TIME_H
+#include <iostream>
 
+namespace seneca{
+
+class Time {
+    unsigned int minutes; // one mandatory attribute to keep number of minutes
+public:
+  Time& reset();
+  Time(unsigned int min = 0u);
+
+  std::ostream& write(std::ostream& ostr = std::cout) const;
+  std::istream& read(std::istream& istr = std::cin);
+
+  operator unsigned int() const;
+  
+  Time& operator*= (int val);
+  Time& operator-= (const Time& D);
+  Time operator-(const Time& T) const;
+  
+};
+   std::ostream& operator<<(std::ostream& ostr, const Time& time);
+   std::istream& operator>>(std::istream& istr, Time& time);
+}
+
+#endif
