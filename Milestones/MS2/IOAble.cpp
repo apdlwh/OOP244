c51400d11dd3ab03667b0e1a5be9e0c5fc7beaee
apdlwh <146094326+apdlwh@users.noreply.github.com>
2024-03-26 17:08:01 -0400

    Update IOAble.cpp

--- Milestones/MS2/IOAble.cpp
+++ Milestones/MS2/IOAble.cpp
@@ -1,1 +1,15 @@
+#include "IOAble.h"
 
+namespace seneca {
+
+    std::ostream& operator<<(std::ostream& os, const IOAble& io)
+    {
+        return io.write(os);
+    }
+    std::istream& operator>>(std::istream& is, IOAble& io) 
+    {
+        return io.read(is);
+    }
+
+
+}
