0b283b3cd988e74bc30526f17641ca23f80adae9
apdlwh <146094326+apdlwh@users.noreply.github.com>
2024-03-26 17:08:19 -0400

    Update IOAble.h

--- Milestones/MS2/IOAble.h
+++ Milestones/MS2/IOAble.h
@@ -1,1 +1,19 @@
+#ifndef IOABLE_H
+#define IOABLE_H
+#include <iostream>
+namespace seneca{
 
+class IOAble{
+
+public:
+     virtual ~IOAble() {}
+     virtual std::ostream& write(std::ostream& os) const = 0;
+     virtual std::istream& read(std::istream& is) = 0;
+};
+     
+     std::ostream& operator<<(std::ostream& os, const IOAble& io);
+     std::istream& operator>>(std::istream& is, IOAble& io);
+    
+}
+
+#endif
